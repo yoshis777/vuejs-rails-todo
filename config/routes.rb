c3e99d6650ac794049e47e638e637fb2032fdce7
yoshis777 <alt0prom@gmail.com>
2019-05-08 18:33:48 +0900

    railsでvue.jsのCRUD用にREST API作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  namespace :api, format: 'json' do
+    resources :tasks, only: [:index, :create, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_08_081519) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "is_done", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
